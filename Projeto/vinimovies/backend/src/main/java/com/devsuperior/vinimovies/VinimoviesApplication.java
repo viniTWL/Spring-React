fdf8a19eebf803f375ac00df23fe19b7caab9ef4
viniTWL <vinigdc12@gmail.com>
2022-01-10 22:14:04 -0300

    Creating Workspace

---
+++ Projeto/vinimovies/backend/src/main/java/com/devsuperior/vinimovies/VinimoviesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.vinimovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinimoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinimoviesApplication.class, args);
+	}
+
+}
---
+++ Projeto/vinimovies/backend/src/test/java/com/devsuperior/vinimovies/VinimoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.vinimovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinimoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

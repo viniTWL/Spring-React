312c8841df9d2253a72435643e57c1b8b948b51a
viniTWL <vinigdc12@gmail.com>
2022-01-12 16:15:49 -0300

    Routes
    Configurando rotas do projeto

--- Projeto/vinimovies/frontend/src/App.tsx
+++ Projeto/vinimovies/frontend/src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import {ReactComponent as GithubIcon} from './assets/img/github.svg';
 import Navbar from './components/Navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
 
-function App() {
+export default function App() {
   return (
-    <Navbar />
-  )
-  ;
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
---
+++ Projeto/vinimovies/frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Listing(){
+    return(
+        <h1>Listagem</h1>
+    )
+}

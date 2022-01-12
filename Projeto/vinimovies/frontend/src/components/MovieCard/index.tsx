ff5b830a122d7eef76e18211b6d17505b439b448
viniTWL <vinigdc12@gmail.com>
2022-01-12 18:03:58 -0300

    Navegation Buttons
    Colocando as rotas dos botão de navegação

--- Projeto/vinimovies/frontend/src/components/MovieCard/index.tsx
+++ Projeto/vinimovies/frontend/src/components/MovieCard/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MovieScore from "components/MovieScore";
+import { Link } from "react-router-dom";
 
 export default function MovieCard(){
 
@@ -17,7 +18,10 @@ export default function MovieCard(){
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <MovieScore />
-                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                
+                <Link to={`/form/${movie.id}`}>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                </Link>
             </div>
         </div>
     )
--- Projeto/vinimovies/frontend/src/pages/Form/index.tsx
+++ Projeto/vinimovies/frontend/src/pages/Form/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './style.css';
+import { Link } from "react-router-dom";
 
 export default function Form(){
 
@@ -14,30 +15,32 @@ export default function Form(){
 
     return(
         <div className="dsmovie-form-container">
-    <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title}/>
-    <div className="dsmovie-card-bottom-container">
-        <h3>{movie.title}</h3>
-        <form className="dsmovie-form">
-            <div className="form-group dsmovie-form-group">
-                <label htmlFor="email">Informe seu email</label>
-                <input type="email" className="form-control" id="email" />
-            </div>
-            <div className="form-group dsmovie-form-group">
-                <label htmlFor="score">Informe sua avaliação</label>
-                <select className="form-control" id="score">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
-            </div>
-            <div className="dsmovie-form-btn-container">
-                <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
-            </div>
-        </form >
-        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
-    </div >
-</div >
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title}/>
+            <div className="dsmovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <form className="dsmovie-form">
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="email">Informe seu email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="score">Informe sua avaliação</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="dsmovie-form-btn-container">
+                        <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                    </div>
+                </form >
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
+            </div >
+        </div >
     )
 }
